8028907db86db419c122ba6fcf66d15ffb4dd966
Michu <michal.dakowicz@infotech.edu.pl>
2024-05-20 18:13:12 +0200

    icon renames settings implementation and more themes

--- fletapp.py
+++ fletapp.py
@@ -10,7 +10,9 @@ if not os.path.exists('notes.json'):
 
 class Colors:
     def __init__(self):
-        self.BG = '#343a40'
+        self.COLOR_THEME = 'Blue'
+        self.BG = '#1A1C1E'
+        self.BGACCENT = '#151618'
         self.TEXT = '#ffffff'
         self.PRIMARY = '#007bff'
         self.SECONDARY = '#0056b3'
@@ -21,11 +23,40 @@ class Colors:
     
     def setLight(self):
         self.BG = '#f8f9fa'
+        self.BGACCENT = '#e9ecef'
         self.TEXT = '#000000'
         
     def setDark(self):
-        self.BG = '#343a40'
+        self.BG = '#1A1C1E'
+        self.BGACCENT = '#151618'
         self.TEXT = '#ffffff'
+        
+    def setBlue(self):
+        self.COLOR_THEME = 'Blue'
+        self.PRIMARY = '#007bff'
+        self.SECONDARY = '#0056b3'
+        self.SUCCESS = '#28a745'
+        self.DANGER = '#dc3545'
+        self.WARNING = '#ffc107'
+        self.INFO = '#17a2b8'
+    
+    def setOlive(self):
+        self.COLOR_THEME = 'Olive'
+        self.PRIMARY = '#661b04'
+        self.SECONDARY = '#8a2e00'
+        self.SUCCESS = '#28a745'
+        self.DANGER = '#dc3545'
+        self.WARNING = '#ffc107'
+        self.INFO = '#17a2b8'
+        
+    def setSunset(self):
+        self.COLOR_THEME = 'Sunset'
+        self.PRIMARY = '#ff4500'
+        self.SECONDARY = '#ff8c00'
+        self.SUCCESS = '#28a745'
+        self.DANGER = '#dc3545'
+        self.WARNING = '#ffc107'
+        self.INFO = '#17a2b8'
         
 colors = Colors()
     
@@ -47,7 +78,24 @@ def main(page: ft.Page):
             notes_data = json.load(f)
             return [Note(n['title'], n['content'], n['note_id']) for n in notes_data]
 
-        
+    def open_settings(e):
+        def set_light_mode(e):
+            colors.setLight()
+            page.theme_mode = 'light'
+            open_settings(e)
+            
+        def set_dark_mode(e):
+            colors.setDark()
+            page.theme_mode = 'dark'
+            open_settings(e)
+                        
+        page.clean()
+        page.add(ft.AppBar(ft.Image(src=f"logo-transparent.png"), title=ft.Text('Settings', color=colors.TEXT), actions=[ft.IconButton(ft.icons.CLOSE, on_click=lambda e: main(page))], bgcolor=colors.BGACCENT, color=colors.TEXT, elevation=0))
+        page.add(ft.Row([ft.IconButton(ft.icons.LIGHT_MODE, on_click=set_light_mode), ft.Text("Light Mode"), ft.IconButton(ft.icons.DARK_MODE, on_click=set_dark_mode), ft.Text("Dark Mode")]))
+        page.add(ft.Divider())
+        page.add(ft.Text(f'Current Color Theme is {colors.COLOR_THEME}', color=colors.TEXT))
+        page.add(ft.Row([ft.CupertinoButton('Blue Theme', bgcolor='#007bff', color=colors.TEXT, on_click=lambda e: (colors.setBlue(), open_settings(e))), ft.CupertinoButton('Olive Theme', bgcolor='#661b04', color=colors.TEXT, on_click=lambda e: (colors.setOlive(), open_settings(e))), ft.CupertinoButton('Sunset Theme', bgcolor='#ff4500', color=colors.TEXT, on_click=lambda e: (colors.setSunset(), open_settings(e)))]))
+
     def load_note(note_info):
         def save_note(e):
             title = note_title.value
@@ -87,9 +135,13 @@ def main(page: ft.Page):
             return 
         for note in notes:
             if note.title:
-                page.add(ft.Row([ft.CupertinoButton(note.title, bgcolor=colors.SECONDARY, color=colors.TEXT, on_click=lambda e: load_note(note)), ft.TextField(note.content, border=ft.InputBorder.NONE, multiline=True, min_lines=1, max_lines=3, hint_text='No content' if not note.content else None, disabled=True, width=(page.width - 250), color=colors.TEXT)]))
+                page.add(ft.Row([ft.CupertinoButton(note.title, bgcolor=colors.SECONDARY, color=colors.TEXT, on_click=lambda e: load_note(note)), 
+                                 ft.TextField(note.content, border=ft.InputBorder.NONE, multiline=True, min_lines=1, max_lines=3, 
+                                              hint_text='No content' if not note.content else None, disabled=True, width=(page.width - 250), color=colors.TEXT)]))
             else:
-                page.add(ft.Row([ft.CupertinoButton('Untitled', bgcolor=colors.SECONDARY, color=colors.TEXT, on_click=lambda e: load_note(note)), ft.TextField(note.content, border=ft.InputBorder.NONE, multiline=True, min_lines=1, max_lines=3, hint_text='No content' if not note.content else None, disabled=True, width=(page.width - 250), color=colors.TEXT)]))
+                page.add(ft.Row([ft.CupertinoButton('Untitled', bgcolor=colors.SECONDARY, color=colors.TEXT, on_click=lambda e: load_note(note)), 
+                                 ft.TextField(note.content, border=ft.InputBorder.NONE, multiline=True, min_lines=1, max_lines=3, 
+                                              hint_text='No content' if not note.content else None, disabled=True, width=(page.width - 250), color=colors.TEXT)]))
             page.add(ft.Divider())
     
     def new_note(e):
@@ -122,11 +174,13 @@ def main(page: ft.Page):
         page.add(ft.CupertinoButton('Save', bgcolor=colors.PRIMARY, color=colors.TEXT, on_click=save_note))
         page.add(ft.CupertinoButton('Cancel', bgcolor=colors.PRIMARY, color=colors.TEXT, on_click=lambda e: main(page)))
         page.add(ft.CupertinoButton('Delete', bgcolor=colors.PRIMARY, color=colors.TEXT, on_click=delete_note))
-        
     
     page.clean()
     
     page.title = "Notey"
+    page.add(ft.AppBar(ft.Image(src=f"logo-transparent.png"), title=ft.Text('Notey', color=colors.TEXT), actions=[
+        ft.IconButton(ft.icons.SETTINGS, on_click=open_settings)
+        ], bgcolor=colors.BGACCENT, color=colors.TEXT, elevation=0))
     page.add(ft.FloatingActionButton(icon=ft.icons.ADD, on_click=new_note, bgcolor=colors.PRIMARY))
     page.add(ft.Stack())
     
